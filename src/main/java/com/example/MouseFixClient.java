0f8f356c8bf315374593bd39275bbe9b5614deca
gai28112017-oss <gai28112017@gmail.com>
2026-08-10 18:23:24 +0700

    Update MouseFixClient.java

--- src/main/java/com/example/MouseFixClient.java
+++ src/main/java/com/example/MouseFixClient.java
@@ -1,1 +1,11 @@
+package com.example;
 
+import net.fabricmc.api.ClientModInitializer;
+
+public class MouseFixClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        System.out.println("[ZTE Mouse Fix] Client loaded!");
+    }
+}
